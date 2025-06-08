4d18f20bcd7a23cb256c29554feb99205a14a281
Igor Stevanovic <igor.d.stevanovic@outlook.com>
2025-06-08 08:41:12 +0200

    Set headless to True

--- features/environment.py
+++ features/environment.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 
 def before_all(context):
     context.playwright = sync_playwright().start()
-    context.browser = context.playwright.chromium.launch(headless=False)
+    context.browser = context.playwright.chromium.launch(headless=True)
 
 def before_scenario(context, scenario):
     print("before_scenario: creating page")
