b52685babeaef76a9227dc1e6f3b509d2eef9cfe
Igor Stevanovic <igor.d.stevanovic@outlook.com>
2025-06-08 11:57:16 +0200

    Updated

--- features/environment.py
+++ features/environment.py
@@ -2,11 +2,15 @@ from playwright.sync_api import sync_playwright
 
 def before_all(context):
     context.playwright = sync_playwright().start()
-    context.browser = context.playwright.chromium.launch(headless=True)
+    context.browser = context.playwright.chromium.launch(
+        headless=True,
+        args=["--start-maximized"]
+    )
 
 def before_scenario(context, scenario):
     print("before_scenario: creating page")
-    context.page = context.browser.new_page()
+    # Set viewport to None to allow window to be maximized
+    context.page = context.browser.new_page(viewport=None)
 
 def after_scenario(context, scenario):
     context.page.close()
